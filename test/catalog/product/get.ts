3ca56a3ae3fdd8561dfe521802d898191aeb0901
Igor Ludgero Miura <igor@imaginationmedia.com>
2024-03-11 10:59:28 -0300

    Update package.json and add test

---
+++ test/catalog/product/get.ts
@@ -0,0 +1,20 @@
+import { default as MagentoApi } from '../../../src/index';
+
+(async () => {
+    let client = new MagentoApi({
+        'url': '',
+        'consumerKey': '',
+        'consumerSecret': '',
+        'accessToken': '',
+        'tokenSecret': ''
+    });
+
+    let sku = "65-813";
+
+    try {
+        let response = await client.get(`products/${sku}`);
+        console.log(JSON.stringify(response.data));
+    } catch (e) {
+        console.log(e);
+    }
+})();
